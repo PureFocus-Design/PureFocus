ad50f875d9e0925f8abed69269bc7b129f7fdd89
Ryan Dines <ryan.dines@gmail.com>
2017-08-23 09:55:58 -0400

    Committing before trying something new.

--- PureFocus/Alamo.swift
+++ PureFocus/Alamo.swift
@@ -7,6 +7,25 @@
 //
 
 import Foundation
+import Alamofire
+
+class AlamoNetwork{
+    
+    func makeXMLHeaders()->String{
+        
+        let xmlheader1 = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+        let xmlheader2 = "<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">"
+        let xmlheader3 = "<plist version=\"1.0\">"
+        
+        return xmlheader1+xmlheader2+xmlheader3
+    }
+    
+    func clientCheckIn(){
+        
+    }
+    
+    
+}
 
 
 
